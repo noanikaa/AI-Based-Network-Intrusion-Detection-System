3b2737caefaa71f57edbeede2a14d477816b8a03
noanikaa <117883375+noanikaa@users.noreply.github.com>
2024-06-15 22:49:31 +0530

    Create Given_PLT_1.py

---
+++ Given_PLT_1.py
@@ -0,0 +1,80 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LogisticRegression 
+from sklearn.svm import SVC 
+from sklearn.metrics import accuracy_score 
+from sklearn.preprocessing import MinMaxScaler 
+from sklearn.manifold import TSNE 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+from keras.layers import Input, Dense 
+from keras.models import Model, Sequential 
+from keras import regularizers
+import pandas as pd
+import numpy
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, zero_one_loss
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn import metrics
+
+
+
+features = ['duration','protocol_type','service','flag','src_bytes','dst_bytes','land','wrong_fragment','urgent','hot','num_failed_logins','logged_in','num_compromised','root_shell','su_attempted',
+'num_root','num_file_creations','num_shells','num_access_files','num_outbound_cmds',
+'is_host_login',
+'is_guest_login',
+'count',
+'srv_count',
+'serror_rate',
+'srv_serror_rate',
+'rerror_rate',
+'srv_rerror_rate',
+'same_srv_rate',
+'diff_srv_rate',
+'srv_diff_host_rate',
+'dst_host_count',
+'dst_host_srv_count',
+'dst_host_same_srv_rate',
+'dst_host_diff_srv_rate',
+'dst_host_same_src_port_rate',
+'dst_host_srv_diff_host_rate',
+'dst_host_serror_rate',
+'dst_host_srv_serror_rate',
+'dst_host_rerror_rate',
+'dst_host_srv_rerror_rate',
+'intrusion_type']
+
+data = pd.read_csv('kddcup.data_10_percent_corrected', names=features, header=None)
+data.head()
+output = data['intrusion_type'].values
+labels = set(output)
+duplicateRowsDF = data[data.duplicated()]
+data.drop_duplicates(subset=features, keep='first', inplace=True)
+data.to_pickle('data.pkl')
+data = pd.read_pickle('data.pkl')
+plt.figure(figsize=(20,15))
+class_distribution = data['intrusion_type'].value_counts()
+class_distribution.plot(kind='bar')
+plt.xlabel('Class')
+plt.ylabel('Data points per Class')
+plt.title('Distribution of Attack')
+plt.grid()
+plt.show()
+
+##def pairplot(data, label, features=[]):
+##    '''
+##    This function creates pairplot taking 4 features from our dataset as default parameters along with the output variable
+##    '''
+##    sns.pairplot(data, hue=label, height=4, diag_kind='hist', vars=features,
+##            plot_kws={'alpha':0.6, 's':80, 'edgecolor':'k'})
+##    plt.show()
+##
+##
+##pairplot(data, 'intrusion_type', features=['duration', 'src_bytes', 'dst_bytes', 'wrong_fragment'])
+##
